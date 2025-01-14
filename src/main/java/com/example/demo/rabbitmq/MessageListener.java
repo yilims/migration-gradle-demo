df1a667832aabbd32ee240e6f6d69b95db29404f
yilims <brightmonkeyli@163.com>
2025-01-14 11:17:37 +0800

    fix compile error

--- src/main/java/com/example/demo/rabbitmq/MessageListener.java
+++ src/main/java/com/example/demo/rabbitmq/MessageListener.java
@@ -1,6 +1,8 @@
 package com.example.demo.rabbitmq;
 
+import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
