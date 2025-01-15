ea13ac93de33fb0a6bfa74ceac44f29f26d7a367
yilims <brightmonkeyli@163.com>
2025-01-15 12:45:32 +0800

    add new method

--- src/main/java/com/example/demo/rabbitmq/MessageListener.java
+++ src/main/java/com/example/demo/rabbitmq/MessageListener.java
@@ -1,6 +1,5 @@
 package com.example.demo.rabbitmq;
 
-import org.springframework.amqp.core.AmqpTemplate;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
