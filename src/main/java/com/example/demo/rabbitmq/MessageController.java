219fd08e7bcbb9875bdc240c123252e3dd749e70
yilims <brightmonkeyli@163.com>
2025-01-15 16:56:42 +0800

    r

--- src/main/java/com/example/demo/rabbitmq/MessageController.java
+++ src/main/java/com/example/demo/rabbitmq/MessageController.java
@@ -1,6 +1,6 @@
 package com.example.demo.rabbitmq;
 
-import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
